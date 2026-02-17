af5b652e7b7f276d8df7e2bc48dd3412c834330b
Surrii1 <alfansuri58@gmail.com>
2026-02-17 20:14:57 +0700

    salonalfan
    salonalfan

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-// COUNTDOWN
-const weddingDate = new Date("Feb 11, 2026 08:00:00").getTime();
+function scrollBooking(){
 
-setInterval(() => {
-  const now = new Date().getTime();
-  const gap = weddingDate - now;
+document.getElementById("booking")
+.scrollIntoView();
 
-  const days = Math.floor(gap / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((gap % (1000 * 60)) / 1000);
-
-  document.getElementById("days").innerText = days;
-  document.getElementById("hours").innerText = hours;
-  document.getElementById("minutes").innerText = minutes;
-  document.getElementById("seconds").innerText = seconds;
-}, 1000);
+}
 
 
-// WISHES
-document.getElementById("wishForm").addEventListener("submit", function(e){
-  e.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const message = document.getElementById("message").value;
+function pilihLayanan(nama,harga){
 
-  const newWish = document.createElement("p");
-  newWish.innerHTML = `<strong>${name}:</strong> ${message}`;
+document.getElementById("layananInput")
+.value=nama;
 
-  document.getElementById("wishList").appendChild(newWish);
+document.getElementById("hargaInput")
+.value="Rp "+harga;
 
-  this.reset();
-});
+}
+
+
+
+function booking(){
+
+let nama=document.getElementById("nama").value;
+
+let layanan=document.getElementById("layananInput").value;
+
+let tanggal=document.getElementById("tanggal").value;
+
+let jam=document.getElementById("jam").value;
+
+
+if(nama=="" || layanan==""){
+
+alert("Isi data dulu");
+
+}else{
+
+alert("Booking berhasil atas nama "+nama);
+
+}
+
+}
